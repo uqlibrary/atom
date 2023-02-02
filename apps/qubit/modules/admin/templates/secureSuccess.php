221ae2f29dcb4bdb3bcdf0ed4ab663dd98b397f6
Rohan Ferris <r.ferris@library.uq.edu.au>
2023-02-02 13:52:41 +1000

    Add link to fryer request form

--- apps/qubit/modules/admin/templates/secureSuccess.php
+++ apps/qubit/modules/admin/templates/secureSuccess.php
@@ -3,7 +3,10 @@
   <h2><?php echo __('Sorry, you do not have permission to access that page'); ?></h2>
 
   <div class="tips">
-    <p><a href="javascript:history.go(-1)"><?php echo __('Back to previous page'); ?></a></p>
+     <p><?php echo __('To request access to this resource please use the ') ?>
+       <a href="https://web.library.uq.edu.au/library-services/special-collections/fryer-item-request-form"><?php echo __("Fryer item request form.") ?></a>
+     </p>
+     <p><a href="javascript:history.go(-1)"><?php echo __('Back to previous page'); ?></a></p>
     <p><?php echo link_to(__('Go to homepage'), '@homepage'); ?></p>
   </div>
 
