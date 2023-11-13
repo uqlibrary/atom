f591c99eed8d90dfdf8f6fd8fdc06051a16c803d
Rohan Ferris <r.ferris@library.uq.edu.au>
2023-11-13 13:20:04 +1000

    change Search holdings to Search

--- apps/qubit/modules/repository/templates/_holdings.php
+++ apps/qubit/modules/repository/templates/_holdings.php
@@ -5,7 +5,7 @@
 <form class="sidebar-search" action="<?php echo url_for(array('module' => 'informationobject', 'action' => 'browse')) ?>">
   <input type="hidden" name="repos" value="<?php echo $resource->id ?>">
   <div class="input-prepend input-append">
-    <input type="text" name="query" placeholder="<?php echo __('Search holdings') ?>">
+    <input type="text" name="query" placeholder="<?php echo __('Search') ?>">
     <button class="btn" type="submit">
       <i class="fa fa-search"></i>
     </button>
